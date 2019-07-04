4cfdb9f2e58ee26237c77c3f25cacb0d37718138
Michael Olszewski <michal.olszewski012@wp.pl>
2019-07-04 11:42:05 +0200

    Wstepna strona

--- Thea2Translator.WebPage/src/app/app.module.ts
+++ Thea2Translator.WebPage/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule 
   ],
   providers: [],
   bootstrap: [AppComponent]

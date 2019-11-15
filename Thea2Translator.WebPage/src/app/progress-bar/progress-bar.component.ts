db7e0e77d01376698dc0eaa5f07a48e56fb0faf1
Michael Olszewski <michal.olszewski012@wp.pl>
2019-11-15 16:52:39 +0100

    Aktualizacja strony

--- Thea2Translator.WebPage/src/app/progress-bar/progress-bar.component.ts
+++ Thea2Translator.WebPage/src/app/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressBarComponent implements OnInit {
 
+  @Input("value") value: number = 0;
+  @Input("firstGradient") firstGradient;
+  @Input("secondGradient") secondGradient;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setStyle() {
+    let borderRadius = 0;
+
+    if (this.value > 90 && this.value <= 97)
+      borderRadius = 30;
+    else if (this.value > 97)
+      borderRadius = 50;
+
+    let styles = {
+      'background-color': this.firstGradient,
+      'background-image': 'linear-gradient(to bottom right, ' + this.firstGradient + ', ' + this.secondGradient + ')',
+      'height': '15px',
+      'border-radius': '50px ' + borderRadius + 'px ' + borderRadius + 'px 50px',
+      'width': this.value + '%'
+    };
+    return styles;
   }
 
 }
--- Thea2Translator.WebPage/src/app/status/status.component.ts
+++ Thea2Translator.WebPage/src/app/status/status.component.ts
@@ -10,24 +10,52 @@ import { Status } from '../_model/status';
 })
 export class StatusComponent implements OnInit {
 
-  databaseStatus:StatusRecord;
-  modulesStatus:StatusRecord;
+  databaseStatus: StatusRecord;
+  modulesStatus: StatusRecord;
 
-  constructor(private statusService:StatusService) { }
+  modifiedDateForDatabase = "";
+  modifiedDateForModules = "";
+
+  dateForDisplay = "";
+
+  constructor(private statusService: StatusService) { }
 
   ngOnInit() {
-
-    this.statusService.getDatabaseStatus().subscribe(data =>{
-      var record:StatusRecord = JSON.parse(data.toString());
+    this.statusService.getDatabaseStatus().subscribe(data => {
+      var record: StatusRecord = JSON.parse(data.toString());
       this.databaseStatus = record;
-      console.log(this.databaseStatus);
+     
+      this.modifiedDateForDatabase = record.modifiedDate;
+      this.onStatusChange();
     });
 
-    this.statusService.getModulesStatus().subscribe(data =>{
-      var record:StatusRecord = JSON.parse(data.toString());
+    this.statusService.getModulesStatus().subscribe(data => {
+      var record: StatusRecord = JSON.parse(data.toString());
       this.modulesStatus = record;
-      console.log(this.modulesStatus);
+
+      this.modifiedDateForModules = record.modifiedDate;
+      this.onStatusChange();    
     });
+  }
+
+  onStatusChange() {
+    if (this.modifiedDateForDatabase == "") {
+      this.dateForDisplay = this.modifiedDateForModules;
+    }
+    else if (this.modifiedDateForModules == "") {
+      this.dateForDisplay = this.modifiedDateForDatabase;
+    }
+    else {
+      var date1 = new Date(this.modifiedDateForDatabase);
+      var date2 = new Date(this.modifiedDateForModules);
+
+      if(date1 > date2){
+        this.dateForDisplay = this.modifiedDateForDatabase;
+      }
+      else{
+        this.dateForDisplay = this.modifiedDateForModules;
+      }
+    }
   }
 
 }
